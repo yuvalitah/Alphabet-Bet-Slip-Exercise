cddb5bfac40c8b1e5a6687cbd9584d0a7cadb22b
Yuval Itah <yuval18998@gmail.com>
2022-12-14 00:03:51 +0200

    Add the badge in drawer icon for the amound of selected bets

--- src/components/drawer/DrawerIcon.tsx
+++ src/components/drawer/DrawerIcon.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { IconButton } from "@mui/material";
+import { IconButton, Badge } from "@mui/material";
 import CasinoIcon from "@mui/icons-material/Casino";
+import { useBets } from "../../hooks";
 
 interface IDrawerIconProps {
   handleDrawerToggle: () => void;
 }
 
-export const DrawerIcon = ({ handleDrawerToggle }: IDrawerIconProps) => (
-  <IconButton onClick={handleDrawerToggle}>
-    <CasinoIcon fontSize="large" />
-  </IconButton>
-);
+export const DrawerIcon = ({ handleDrawerToggle }: IDrawerIconProps) => {
+  const bets = useBets();
+
+  return (
+    <IconButton onClick={handleDrawerToggle}>
+      <Badge color="secondary" badgeContent={bets.length}>
+        <CasinoIcon fontSize="large" />
+      </Badge>
+    </IconButton>
+  );
+};
